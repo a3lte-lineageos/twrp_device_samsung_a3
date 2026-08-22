c1bb1c704fa610ef5017bece2830f122d2891900
nnc <notnoelchannel1@protonmail.com>
2026-08-22 10:40:59 +0200

    a33g

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
 TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_a3_eur_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_a33g_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
 
